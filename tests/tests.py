c60120d30e0cf276dff9483229f3179590fa210d
qwatro1111 <mr.outstanding111@gmail.com>
2019-10-07 10:02:41 +0300

    small typo

--- tests/tests.py
+++ tests/tests.py
@@ -10,19 +10,19 @@ class Test(unittest.TestCase):
         self.rectangle = Rectangle(self.width, self.height)
 
     def test_1_rectangle_perimeter(self):
-        cheack = (self.width+self.height)*2
+        cheak = (self.width+self.height)*2
         result = self.rectangle.get_rectangle_perimeter()
-        self.assertEqual(cheack, result)
+        self.assertEqual(cheak, result)
 
     def test_2_rectangle_square(self):
-        cheack = self.width*self.height
+        cheak = self.width*self.height
         result = self.rectangle.get_rectangle_square()
-        self.assertEqual(cheack, result)
+        self.assertEqual(cheak, result)
 
     def test_3_sum_of_corners_valid(self):
-        cheack = 4*90
+        cheak = 4*90
         result = self.rectangle.get_sum_of_corners(4)
-        self.assertEqual(cheack, result)
+        self.assertEqual(cheak, result)
 
     def test_4_sum_of_corners_invalid(self):
         with self.assertRaises(ValueError):
@@ -33,20 +33,20 @@ class Test(unittest.TestCase):
             self.rectangle.get_sum_of_corners(0)
 
     def test_6_rectangle_diagonal(self):
-        cheack = math.sqrt(self.width**2 + self.height**2)
+        cheak = math.sqrt(self.width**2 + self.height**2)
         result = self.rectangle.get_rectangle_diagonal()
-        self.assertEqual(cheack, result)
+        self.assertEqual(cheak, result)
 
     def test_7_radius_of_circumscribed_circle(self):
-        cheack = self.rectangle.get_rectangle_diagonal() / 2
+        cheak = self.rectangle.get_rectangle_diagonal() / 2
         result = self.rectangle.get_radius_of_circumscribed_circle()
-        self.assertEqual(cheack, result)
+        self.assertEqual(cheak, result)
 
     def test_8_radius_of_inscribed_circle(self):
         rectangle = Rectangle(width=1, height=1)
-        cheack = rectangle.get_rectangle_diagonal() / 2 * math.sqrt(2)
+        cheak = rectangle.get_rectangle_diagonal() / 2 * math.sqrt(2)
         result = rectangle.get_radius_of_inscribed_circle()
-        self.assertEqual(cheack, result)
+        self.assertEqual(cheak, result)
 
     def test_9_radius_of_inscribed_circle(self):
         with self.assertRaises(ValueError):
